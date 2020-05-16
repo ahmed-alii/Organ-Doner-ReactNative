dcd2280f02ee05f69464c9761a024e6aa064901e
Mian442 <62568869+Mian442@users.noreply.github.com>
2020-05-16 23:28:21 +0500

    Commented

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import Categorylist from "../components/Category/Categorylist";
 
 export default ({ navigation }) => {
+  //This file is used to render the category Menu of the Donation-Type
   return (
     <View style={{ flex: 1 }}>
       <Categorylist navigation={navigation} />
